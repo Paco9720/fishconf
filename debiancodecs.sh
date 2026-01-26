69559e96c394ee433b2375b29c61b6e33ddde89c
paco9720 <62359183+Paco9720@users.noreply.github.com>
2026-01-25 23:07:17 -0600

    Add script to install multimedia codecs

---
+++ debiancodecs.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install \
+ffmpeg \
+libavcodec-extra\
+gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-bad \
+gstreamer1.0-plugins-ugly \
+gstreamer1.0-libav
